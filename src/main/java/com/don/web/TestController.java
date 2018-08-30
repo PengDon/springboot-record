5f243921711d8bf937c08185d37095b02f3d7abd
tangp <tangp@dtds.com.cn>
2018-08-30 15:46:31 +0800

    添加Thymeleaf模板引擎实例

--- src/main/java/com/don/web/TestController.java
+++ src/main/java/com/don/web/TestController.java
@@ -1,10 +1,20 @@
 package com.don.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+// 用模板时不用@RestController注解，因为它默认返回的JSON格式
+@Controller
+@RequestMapping("/")
 public class TestController {
+
+    @RequestMapping("/home")
+    public String index(ModelMap map) {
+        map.addAttribute("home","welcome to home");
+        return "index";
+    }
 
     @RequestMapping("/test")
     public String test(){
