09b17166717db2afc01fabc4c8f2e8110aeee80b
tangp <tangp@dtds.com.cn>
2018-08-31 09:52:18 +0800

    添加Spring Security支持

--- src/main/java/com/don/web/TestController.java
+++ src/main/java/com/don/web/TestController.java
@@ -24,13 +24,21 @@ public class TestController {
         return "this is test";
     }
 
+    @ApiOperation(value="默认异常处理", notes="")
     @RequestMapping(value = "/jmm",method = RequestMethod.GET)
     public String jmm() throws Exception{
        throw new Exception("发生错误");
     }
 
+    @ApiOperation(value="自定义异常处理", notes="")
     @RequestMapping("/json")
     public String json() throws MyException {
         throw new MyException("发生错误2");
+    }
+
+    @ApiOperation(value="登录", notes="")
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
 }
